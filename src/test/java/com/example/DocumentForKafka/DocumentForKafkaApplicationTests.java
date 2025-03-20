fc2e1a2494b13f0e6b0a9b5e546091adfcdadb0d
lvpolozo <lvpolozo@mts.ru>
2025-03-20 10:42:47 +0300

    add hidden environment-variables change configurations

--- src/test/java/com/example/DocumentForKafka/DocumentForKafkaApplicationTests.java
+++ src/test/java/com/example/DocumentForKafka/DocumentForKafkaApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.DocumentForKafka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DocumentForKafkaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//    @Test
+//	void contextLoads() {
+//
+//	}
 
 }
